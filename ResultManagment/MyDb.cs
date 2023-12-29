0e489b7c510ddcfce93f7f37f9cdf0bdd36f6075
XPK2 <huyvuquang2003@gmail.com>
2023-12-29 14:26:24 +0700

    finalpr

---
+++ ResultManagment/MyDb.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace ResultManagement
+{
+    internal class MyDb
+    {
+        //the connection
+        SqlConnection conn=new SqlConnection("Data Source=DESKTOP-TCT51PT;Initial Catalog=RadDBConnection;Integrated Security=True");
+
+
+        //create a function to get a connection
+        public SqlConnection getConnection
+        {
+            get 
+            { 
+            
+            return conn;
+            
+            }
+        
+        
+        }
+
+        //create a function a to open the connection
+        public void openConnection() 
+        { 
+        
+                if (conn.State==ConnectionState.Closed) {
+                
+                    conn.Open();
+            }
+        
+        }
+
+
+        //create a function a to close the connection
+        public void closeConnection()
+        {
+
+            if (conn.State == ConnectionState.Open)
+            {
+
+                conn.Close();
+            }
+
+        }
+    }
+}
